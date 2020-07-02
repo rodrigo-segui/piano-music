c0f3cb6f89993c514a680b9170572666c91aeb96
Rodrigo-Segui <rodrigo_segui@hotmail.com>
2020-07-02 15:49:38 -0300

    create piano

---
+++ main.js
@@ -0,0 +1,19 @@
+// get all keys
+const keys = document.querySelectorAll('.key')
+
+// play notes
+function playNote(event){
+// keycode
+
+// typed or pressed key
+
+//if key exists
+
+// play audio
+}
+
+ 
+//click with mouse
+
+//keyboard type
+window.addEventListener("keydown", playNote)
